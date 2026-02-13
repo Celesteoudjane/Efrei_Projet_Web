53fcb047408ed6de1e95eb73b5917eb973b11686
Celesteoudjane <celeste.oudjane@efrei.net>
2026-02-13 15:15:15 +0100

    Nous retrouvé ajouter

---
+++ include/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <header>
+        <h1>Département Informatique Efrei</h1>
+        <nav> 
+            <a href="./index.php">Accueil</a>
+            <a href="./equipe.php">Equipe Enseigante</a>
+            <a href="./cours.php">Cours et Formations</a>
+            <a href="./contact">Contact</a>
+</nav>
+        
+    
