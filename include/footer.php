c9d0ea4fd2b2143bb39eb069504e6e1b5741186e
diallo aissatou <aissatou.diallo@efrei.net>
2026-02-13 16:42:26 +0100

    style.css du footer

--- include/footer.php
+++ include/footer.php
@@ -2,6 +2,8 @@
 <html lang="fr">
 <footer>
    <p>©️ 2026 EFREI - Tous droits réservés</p>
+   <a href="https://www.instagram.com/efrei_paris?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+   <img src="images/insta.png" alt="logo insta" class="logo-insta"></a>
     <a href="./a-propos.php">à propos</a>
     <p>Céleste Oudjane - Aissatou Diallo</p>
     <p>Nous retrouvez</p>
