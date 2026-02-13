00ce40ad077f79af8e78734f4971041acee32b83
diallo aissatou <aissatou.diallo@efrei.net>
2026-02-13 15:26:58 +0100

    creation footer

---
+++ include/footer.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="fr">
+<footer>
+   <p>©️ 2026 EFREI - Tous droits réservés</p>
+    <a href="./a-propos.php">à propos</a>
+    <p>Céleste Oudjane - Aissatou Diallo</p>
+    <p>Nous retrouvez</p>
+</footer>
