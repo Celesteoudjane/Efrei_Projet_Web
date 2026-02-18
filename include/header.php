d5af8e7a41273f8a67b8e29b2f8dc2d3c55119f6
Celesteoudjane <celeste.oudjane@efrei.net>
2026-02-19 00:03:18 +0100

    add images and update style

--- include/header.php
+++ include/header.php
@@ -1,15 +1,55 @@
 <!DOCTYPE html>
 <html lang="fr">
     <header class="banner">
-        <h1 class="title">Département Informatique Efrei</h1>
-
-        <a href="../html/index.php">
-            <img src="../img/logo_efrei.png" alt="logo_efrei_img" height="100px">
-        </a>
-        <nav>
-            <a href="../html/index.php">Accueil</a>
-            <a href="../html/equipe.php">Equipe Enseigante</a>
-            <a href="../html/cours.php">Cours et Formations</a>
-            <a href="../html/contact">Contact</a>
+        <div class="ligne">
+            <h1 class="title">Département Informatique Efrei</h1>
+        </div>
+        <nav class="links">
+            <a href="../html/index.php">
+                <img src="../img/logo_efrei.png" alt="logo_efrei_img" height="100px">
+            </a>
+            <a href="../html/index.php" class="lien">Accueil</a>
+            <a href="../html/equipe.php" class="lien">Equipe Enseigante</a>
+            <a href="../html/cours.php" class="lien">Cours et Formations</a>
+            <a href="../html/contact" class="lien" id="btnexterne">Contact</a>
         </nav>
     </header>
+
+    <body>
+        <h1>Ingénieur en mouvement "travaille travaille travaille" </h1>
+        <div class="bigCarousel" >
+
+        
+            <ul class="carouselle">
+                <li data-accName="Item 1">
+
+                    <img src="../img/carousselle/efrei_battiment1.jpg" alt="efrei_battiment1.jpg" class="img_car">
+                </li>
+                <li data-accName="Item 2">
+
+                    <img src="../img/carousselle/efrei_battiment2.jpg" alt="efrei_battiment2.jpg" class="img_car">
+                </li>
+                <li data-accName="Item 3">
+
+                    <img src="../img/carousselle/efrei_battiment3.jpg" alt="efrei_battiment3.jpg" class="img_car">
+                </li>
+                <li data-accName="Item 4">
+
+                    <img src="../img/carousselle/efrei_battiment4.jpg" alt="efrei_battiment4.jpg" class="img_car">
+                </li>
+            </ul>
+            
+            <div class="carouselTexte" >
+            <p>Cet espace est dédié aux enseignants du Département Informatique de l'Efrei.
+                Gérez vos formations, retrouvez vos collègues et accédez à vos outils pédagogiques
+                en un clic.</p>
+                </div>
+        </div>
+        <div>
+            <h2>Département Informatique</h2>
+            <p>Le département informatique de l’EFREI forme des étudiant·es aux métiers du numérique.
+                On y apprend à coder, créer des applications et comprendre les nouvelles technologies
+                comme l’IA ou la cybersécurité. C’est un endroit pour apprendre, pratiquer et préparer
+                son futur dans l’informatique</p>
+        </div>
+    </body>
