90d4bd61e255b15051e63261e6dad61c5fb00a2c
Celesteoudjane <celeste.oudjane@efrei.net>
2026-02-18 18:29:56 +0100

    Ajout image, modif index et header

---
+++ html/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil<title>
+    <link rel="stylesheet" href="../css/style.css">
+<head>
+    <body class="<?php echo $theme; ?>" >
+    
+    <?php include ("../include/header.php"); ?>
+
+
+</body>
