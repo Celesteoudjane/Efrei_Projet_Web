7c9260b771818b2de4bc21ed716402b6eb3f490b
diallo aissatou <aissatou.diallo@efrei.net>
2026-02-19 10:15:03 +0100

    css

--- html/a-propos.php
+++ html/a-propos.php
@@ -6,18 +6,7 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-
-    <header class="navbar">
-        <div class="logo">efrei</div>
-        <nav>
-            <a href="#">Accueil</a>
-            <a href="#">Equipe Enseignante</a>
-            <a href="#">Cours et Formations</a>
-            <a href="#" class="contact">contact</a>
-        </nav>
-    </header>
-
-    <main class="hero">
+   <main class="hero">
 
         <h1>Information Projet</h1>
         <div class="box info-box"></div>
@@ -29,18 +18,6 @@
         </div>
 
     </main>
-
-    <footer>
-        <div class="footer-top">
-            <p>© 2026 EFREI - Tous droits réservés</p>
-            <p>à propos</p>
-            <p>Céleste Oudjane - Aissatou Diallo</p>
-        </div>
-        <div class="footer-bottom">
-            <p>Nous retrouvez</p>
-        </div>
-    </footer>
-
 </body>
 </html>
 
--- include/footer.php
+++ include/footer.php
@@ -3,7 +3,8 @@
 <footer>
    <p>©️ 2026 EFREI - Tous droits réservés</p>
    <a href="https://www.instagram.com/efrei_paris?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
-   <img src="images/insta.png" alt="logo insta" class="logo-insta"></a>
+   <img src="../img/logo_insta.png" alt="logo_insta.png" class="logo-insta">
+   </a>
     <a href="./a-propos.php">à propos</a>
     <p>Céleste Oudjane - Aissatou Diallo</p>
     <p>Nous retrouvez</p>
