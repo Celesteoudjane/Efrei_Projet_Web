64b0b945fd563ecbcb4ef6e5319e903c42ef7ea9
diallo aissatou <aissatou.diallo@efrei.net>
2026-02-20 17:31:48 +0100

    css nous retrouvez

--- html/nous-retrouver.php
+++ html/nous-retrouver.php
@@ -6,3 +6,37 @@
     <link href="../css/style.css" rel="stylesheet" />
 </head>    
     <?php include ("../include/header.php"); ?>
+    <body>
+
+<main class="page">
+
+    <section class="left">
+        <p class="horaire">Horaire : 7h00 - 21h00</p>
+        <p class="jours">Du lundi au vendredi</p>
+
+       <img id="img4" src="../img/maps_efrei.png" alt="maps_efrei.png" class="img_maps">
+    </section>
+
+    <section class="right">
+        <h1>Nous contacter</h1>
+
+        <div class="bloc">
+            <h2>• Campus Paris</h2>
+            <p>+33 188 289 001</p>
+            <p class="email">site-groupe@efrei.fr</p>
+            <p>30-32 Av. de la République, 94800 Villejuif</p>
+        </div>
+
+        <div class="bloc">
+            <h2>• Campus Bordeaux</h2>
+            <p>+33 582 060 162</p>
+            <p class="email">site-groupe@efrei.fr</p>
+            <p>83 Rue Lucien Faure, 33000 Bordeaux</p>
+        </div>
+    </section>
+
+</main>
+</body>
+<?php include ("../include/footer.php"); ?>
+</html>
+
--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
 <footer>
+    <div class="footer-top">
    <p>©️ 2026 EFREI - Tous droits réservés</p>
-   <a href="https://www.instagram.com/efrei_paris?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
+  
+   <a href="./a-propos.php" class="lien">à propos</a>
+   <span>Céleste Oudjane - Aissatou Diallo</span>
+</div>
+
+<div class="footer-bottom">
+     <a href="https://www.instagram.com/efrei_paris?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">
    <img src="../img/logo_insta.png" alt="logo_insta.png" class="logo-insta">
-   </a>
-    <a href="./a-propos.php" class="lien">à propos</a>
-    <p>Céleste Oudjane - Aissatou Diallo</p>
      <a href="./nous-retrouver.php" class="lien">nous-retrouver</a>
 </footer>
